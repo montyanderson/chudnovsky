56c61d3f04cac44761dbd43cd2cb433080b1a13e
Monty Anderson <monty.linux@gmail.com>
2016-11-05 22:38:05 +0000

    got rid of n and d

--- main.c
+++ main.c
@@ -12,13 +12,12 @@ long double factl(long double n) {
 }
 
 long double chudnovsky() {
-	long double r = 0.00, k, n, d;
+	long double r = 0.00, k;
 	const long double a = powl(640320.00, 3.00);
 
 	for(k = 0; k < 200.00; k += 1) {
-		n = powl(-1.00, k) * factl(6.00 * k) * (545140134.00 * k + 13591409.00);
-		d = factl(3.00 * k) * powl(factl(k), 3.00) * powl(a, k + 0.5);
-		r += n / d;
+		r += (powl(-1.00, k) * factl(6.00 * k) * (545140134.00 * k + 13591409.00)) /
+			 (factl(3.00 * k) * powl(factl(k), 3.00) * powl(a, k + 0.5));
 	}
 
 	r *= 12;
