bee86e316b1c56d254d8a5c4ad8a55c4a6a78b83
Monty Anderson <monty.linux@gmail.com>
2016-11-05 22:36:44 +0000

    factl no longer inline

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 
-inline long double factl(long double n) {
+long double factl(long double n) {
 	long double i, f = 1.00;
 
 	for(i = 1.00; i <= n; i += 1) {
@@ -16,7 +16,7 @@ long double chudnovsky() {
 	const long double a = powl(640320.00, 3.00);
 
 	for(k = 0; k < 200.00; k += 1) {
-		n = powl(-1.00, k) * factl(6.00 * k) * ((545140134.00 * k) + 13591409.00);
+		n = powl(-1.00, k) * factl(6.00 * k) * (545140134.00 * k + 13591409.00);
 		d = factl(3.00 * k) * powl(factl(k), 3.00) * powl(a, k + 0.5);
 		r += n / d;
 	}
